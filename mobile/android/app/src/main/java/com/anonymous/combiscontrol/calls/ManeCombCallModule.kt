64964a841969010a329a1727086b6f5d69484ccd
ErickRFM <55464579+ErickRFM@users.noreply.github.com>
2026-08-09 19:56:07 -0600

    fix(rtc): expose incoming call window lifecycle

--- mobile/android/app/src/main/java/com/anonymous/combiscontrol/calls/ManeCombCallModule.kt
+++ mobile/android/app/src/main/java/com/anonymous/combiscontrol/calls/ManeCombCallModule.kt
@@ -2,6 +2,7 @@ package com.anonymous.combiscontrol.calls
 
 import android.content.Intent
 import android.os.Build
+import com.anonymous.combiscontrol.MainActivity
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
@@ -37,6 +38,24 @@ class ManeCombCallModule(
       promise.resolve(null)
     } catch (error: Exception) {
       promise.reject("call_service_stop_failed", error.message, error)
+    }
+  }
+
+  @ReactMethod
+  fun setIncomingCallWindowActive(active: Boolean, promise: Promise) {
+    val activity = reactContext.currentActivity as? MainActivity
+    if (activity == null) {
+      promise.resolve(false)
+      return
+    }
+
+    activity.runOnUiThread {
+      try {
+        activity.setIncomingCallWindowActive(active)
+        promise.resolve(true)
+      } catch (_: Exception) {
+        promise.resolve(false)
+      }
     }
   }
 }
