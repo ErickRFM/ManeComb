5d3b975ae24f28a8d46c87244b2d496be1ccbc80
ErickRFM <55464579+ErickRFM@users.noreply.github.com>
2026-08-12 11:48:06 -0600

    feat(comms): export peer and cleanup contracts

--- shared/communication/index.ts
+++ shared/communication/index.ts
@@ -1,5 +1,7 @@
 export * from './call-types';
 export * from './call-machine';
 export * from './call-signaling';
+export * from './call-peer';
+export * from './call-cleanup';
 export * from './rtc-clock';
 export * from './chat-types';
